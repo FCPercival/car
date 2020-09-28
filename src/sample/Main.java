b193ce2b6aa3ea16330431dcecb9bf93dc7fe41a
FPercival <44174895+FCPercival@users.noreply.github.com>
2020-09-28 11:33:36 +0200

    added icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,6 +16,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ui_javafx.fxml"));
         primaryStage.setTitle("Car Alpha");
+        Image image=new Image("sample/image.png");
+        primaryStage.getIcons().add(image);
         primaryStage.setScene(new Scene(root, 960, 575));
         primaryStage.show();
     }
